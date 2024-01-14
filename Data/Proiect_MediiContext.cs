b0fc36e6155c9938fa6d8a9059911230969c2ac5
LAURA-DANIELA BOTIȘ <laura.botis@stud.ubbcluj.ro>
2024-01-14 22:04:08 +0200

    populare servicii+angajati

--- Data/Proiect_MediiContext.cs
+++ Data/Proiect_MediiContext.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Proiect_Medii.Models;
 
 namespace Proiect_Medii.Data
 {
     public class Proiect_MediiContext : DbContext
     {
-        public Proiect_MediiContext (DbContextOptions<Proiect_MediiContext> options)
+        public Proiect_MediiContext(DbContextOptions<Proiect_MediiContext> options)
             : base(options)
         {
         }
 
-        public DbSet<Proiect_Medii.Models.Serviciu> Serviciu { get; set; } = default!;
+        public DbSet<Serviciu> Serviciu { get; set; } = default!;
+        public DbSet<Programare>? Programare { get; set; }
+        public DbSet<Angajat>? Angajat { get; set; }
+        public DbSet<Client>? Client { get; set; }
 
-        public DbSet<Proiect_Medii.Models.Programare>? Programare { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
-        public DbSet<Proiect_Medii.Models.Angajat>? Angajat { get; set; }
-
-        public DbSet<Proiect_Medii.Models.Client>? Client { get; set; }
+            // Configure cascade delete for the Serviciu-Programare relationship
+            modelBuilder.Entity<Serviciu>()
+                .HasMany(serviciu => serviciu.Programari)
+                .WithOne(programare => programare.Serviciu)
+                .HasForeignKey(programare => programare.ServiciuID)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
